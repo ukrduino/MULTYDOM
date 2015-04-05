5c4d33a701d64b5df172236500cfb8596898fd38
Sergey <ukrduino@gmail.com>
2015-04-05 20:19:33 +0300

    Первый Тест!!!

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from selenium import webdriver
+import unittest
 
-# Create your tests here.
+
+class MainPage(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://127.0.0.1:8000"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def test_title(self):
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        self.assertEqual(u"MultyDOM - лучшие товары для дома!!!", driver.find_element_by_id("topText").text)
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
